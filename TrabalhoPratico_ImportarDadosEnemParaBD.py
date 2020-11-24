bca58b0abb23222a58468d6aee820a64b8108d70
Fabricio Machado <30887617+fbrcmachado@users.noreply.github.com>
2020-11-23 21:32:58 -0300

    Add files via upload
    Esse arquivo serviu para resolução de alguns problemas propostos como Trabalho Pratico do 1o módulo do Bootcamp de Engenharia de Dados.

---
+++ TrabalhoPratico_ImportarDadosEnemParaBD.py
@@ -0,0 +1,53 @@
+##IMPORTANDO INICIALMENTE AS LIBS QUE SERÃO UTILIZADAS:
+
+import pandas as pd
+import zipfile
+import requests
+from io import BytesIO
+import os
+import gc
+import sqlalchemy
+
+
+##SETANDO ONDE SERÁ O LOCAL EM QUE OS DADOS SERÃO DESCARREGADOS E, CASO NÃO EXISTA, CRIA O DIRETÓRIO
+directory = 'C:/Users/antonio.machado/Desktop/Enem'
+os.makedirs(directory, exist_ok=True)
+
+##SETA A URL DOS ARQUIVOS E FAZ O SEU DOWNLOAD
+url = "http://download.inep.gov.br/microdados/microdados_enem_2019.zip"
+
+filebytes = BytesIO(
+    requests.get(url).content
+)
+
+##EXTRAI OS ARQUIVOS
+myzip = zipfile.ZipFile(filebytes)
+myzip.extractall(directory)
+
+##LIMPA A MEMÓRIA E LÊ O ARQUIVO COM OS DADOS
+print("gc: ", gc.collect())
+df_enem = pd.read_csv('c:/Users/antonio.machado/Desktop/Enem/DADOS/MICRODADOS_ENEM_2019.csv', sep=';', encoding='latin-1')
+
+##FILTRAMOS OS ALUNOS COM RESIDÊNCIA EM MG, COMO QUER A QUESTÃO
+df_enem = df_enem.loc[
+        df_enem.SG_UF_RESIDENCIA =='MG'
+        ]
+
+##SALVAMOS OS DADOS EM UM ARQUIVO .CSV
+df_enem.to_csv('C:/Users/antonio.machado/Desktop/Enem/DADOS/DF_MG_2019.csv', sep=';', index=False, encoding='latin-1')
+
+##IMPORTANDO DADOS PARA O MYSQL
+database_username = 'fbrcmachado'
+database_password = 'fbrc2503'
+database_ip       = 'localhost'
+database_name     = 'dbbi'
+
+engine = sqlalchemy.create_engine('mysql+mysqlconnector://{0}:{1}@{2}/{3}'.format(database_username,
+                                                                                  database_password,
+                                                                                  database_ip,
+                                                                                  database_name))
+    
+
+##INSERE DATAFRAME NO MYSQL
+df_enem.to_sql(name = 'enem_mg_2019', con = engine, index = False, chunksize=1000, if_exists = 'append')
+
